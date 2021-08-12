757bccc81e699b426412439a77b0a59e13290b10
GuilhermeSantos99 <guilhermesanctus6@gmail.com>
2021-08-12 10:46:20 -0300

    Versão inicial.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Insta_ScraperBot
- Bot para coletar usernames no Instagram
+# Scraper_InstaBot
+
+> Status: Em desenvolvimento. ⚠️
+
+## Bot capaz de acessar uma conta no Instagram e salvar os usernames dos seguidores de uma conta alvo.
+
+Esse projeto está longe de terminar, ainda está em fase de testes e precisa da ajuda da comunidade para que possa funcionar melhor. 
+
+### Requisitos
+Esse bot, escrito em Python, utiliza a biblioteca Selenium para as suas automações, em conjunto com o webdriver Geckodriver (Firefox).
++ Editor de código. (Ex: Visual Studio Code ou Pycharm.)
++ Python3.
++ Biblioteca Selenium.
++ WebDriver do Firefox (Geckodriver).
+
+Sou um intusiasta da programação e, infelizmente, não pude dedicar o tempo necessário para completar esse projeto. Por isso, fico aberto à sugestões.
+
+### Modo de uso:
++ Na linha 78 do código, insira o seu usuário e senha. 
++ Insira o usuário alvo da coleta de dados, quando for pedido no terminal.
++ Indique também o número de usernames a serem coletados. 
++ Após isso, o bot irá coletar o número de usernames desejado e salvar em um arquivo de texto. 
+
+
+# Scraper_InstaBot
+
+> Status: Under development. ⚠️
+
+## Bot able to access an Instagram account and save the Followers usernames of a target account.
+
+This project is far from finished, it is still in the testing phase and I need the help of the community to make it work better.
+
+### Requirements
+This bot, written in Python, uses a Selenium library for its automations, in conjunction with or webdriver Geckodriver (Firefox).
++ Code editor. (Ex: Visual Studio Code or Pycharm.)
++ Python3.
++ Selenium Library.
++ Firefox WebDriver (Geckodriver).
+
+I'm a programming enthusiast and unfortunately, I can't even devote the time necessary to finish the project. So, fig open to suggestions.
+
+### How to use:
++ On line 78, enter the code or enter your username and password.
++ Enter the target user from data collection when ordering at the terminal.
++ Also indicate or number of usernames to be collected.
++ After that, either bot will collect or desired number of users and save to a text file.
---
+++ User_ScraperBot.py
@@ -0,0 +1,79 @@
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from random import randint
+from time import sleep
+
+class User_ScraperBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
+
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com/")
+        
+        sleep(randint(5, 6)) # Pausa.
+
+        caixa_usuário = driver.find_element_by_xpath('//input[@name="username"]')
+        caixa_usuário.click() # Clica na caixa "usuário".
+        caixa_usuário.clear() # Limpa a caixa, caso haja algo.
+        caixa_usuário.send_keys(self.username) # Digita o usuário informada.
+        
+        caixa_senha = driver.find_element_by_xpath('//input[@name="password"]')
+        caixa_senha.click() # Clica na caixa "password"
+        caixa_senha.clear() # Limpa a caixa, caso haja algo.
+        caixa_senha.send_keys(self.password) # Digita a senha informada.
+
+        sleep(randint(2, 4)) # Pausa.
+
+        caixa_senha.send_keys(Keys.RETURN) # Aperta o botão "Enter".
+
+        self.raspando_seguidores()
+
+    def raspando_seguidores(self):
+
+        contador = 0 
+        seg_num = int(input('Digite o número de usuários coletados: '))
+        user_alvo = input('Digite o perfil do alvo: @')
+
+        driver = self.driver
+        driver.get(f'https://www.instagram.com/{user_alvo}/')
+
+        sleep(randint(3, 4)) # Pausa.
+
+        driver.find_elements_by_class_name('-nal3')[1].click() # Clica no botão 'Seguidores'.
+        
+        sleep(5) # Pausa.
+
+        pop_up = driver.find_element_by_xpath("//div[@class='isgrP']")
+        scroll = 0
+        
+        while True:
+            self.driver.execute_script(
+                'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', pop_up
+                ) # Rola a página do pop-up para poder carregar os usuários. 
+            
+            sleep(1) # Pausa.
+            scroll += 1
+        
+            hrefs = driver.find_elements_by_tag_name('a') # Procura os elementos da tag "hrefs".
+            usernames = [elem.get_attribute("title") for elem in hrefs]
+            while '' in usernames:
+                usernames.remove('')
+            
+            contador = len(usernames)
+            if contador >= seg_num: 
+                break
+            
+            print(f'{contador} usernames salvos...')
+
+        del usernames[seg_num:]
+
+        with open(f'seguidores_de_@{user_alvo}.txt', 'w') as arquivo:
+            for username in usernames:
+                if username != '':
+                    arquivo.write(str('@' + username) + '\n')           
+
+User_ScraperBot = User_ScraperBot('Insira aqui o seu usuário', 'Insira aqui a sua senha')
+User_ScraperBot.login()
